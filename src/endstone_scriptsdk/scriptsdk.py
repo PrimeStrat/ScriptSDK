185aebdfc49a8aba0174e9a7fd104c9b1fd14d5c
CL TheDreWen <louis.charneux@gmail.com>
2025-11-14 11:33:31 +0100

    Update colorama dependency to a minimum version and fix prefix formatting in ScriptSDK

--- src/endstone_scriptsdk/scriptsdk.py
+++ src/endstone_scriptsdk/scriptsdk.py
@@ -9,7 +9,7 @@ class ScriptSDK(Plugin):
     website = "https://discord.gg/kQU9p4ZHbR"
 
     plugin_mc_prefix = "§e[Script§1SDK] §r"
-    prefix = f"{Fore.YELLOW}[Script{Fore.BLUE}SDK] {Fore.RESET}"
+    prefix = f"{Fore.YELLOW}Script{Fore.BLUE}SDK{Fore.RESET}"
 
     def on_load(self):
         self.logger.info(f'Loaded !')
